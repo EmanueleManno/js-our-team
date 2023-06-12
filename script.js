d70804e4633fd9e3c01c287acf61857c8830f436
EmanueleManno <mannus1994@gmail.com>
2023-06-12 14:18:49 +0200

    Push pre-tutoring

--- script.js
+++ script.js
@@ -45,3 +45,24 @@ members = [
         photo: 'barbara-ramos-graphic-designer.jpg'
     }
 ]
+
+//STAMPO TUTTO L'ARRAY SOTTOFORMA DI TABELLA:
+console.table(members);
+
+//STAMPO TUTTO IN CONSOLE:
+console.log(members);
+
+//STAMPO IN CONSOLE IL NOME, IL RUOLO E LA STRINGA DELLA FOTO TENTATIVO 1:
+console.log(members.firstName + members.role + members.photo);
+
+//STAMPO IN CONSOLE IL NOME, IL RUOLO E LA STRINGA DELLA FOTO TENTATIVO 2:
+console.log(members[firstName] + members[role] + members[photo]);
+
+//STAMPO IN CONSOLE IL NOME, IL RUOLO E LA STRINGA DELLA FOTO TENTATIVO 3:
+console.log(members.firstName[i] + members.role[i] + members.photo[i]);
+
+//STAMPO IN CONSOLE IL NOME, IL RUOLO E LA STRINGA DELLA FOTO TENTATIVO 4:
+const names = members.firstName;
+console.log(names);
+console.log(members.names);
+console.log(members[firstName]);
