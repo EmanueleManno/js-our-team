0ce806f62b1a5207a175510183666ed25a2e47b6
EmanueleManno <mannus1994@gmail.com>
2023-06-12 13:48:32 +0200

    Creazione dell'array di oggetti del team

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
 //VEDO SE FUNZIONA:
 console.log('JS OK');
+
+//CREO UN ARRAY DEI MEMBRI DEL TEAM:
+members = [
+    {
+        firstName: 'Wayne',
+        surName: 'Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        firstName: 'Angela',
+        surName: 'Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        firstName: 'Walter',
+        surName: 'Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        firstName: 'Angela',
+        surName: 'Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        firstName: 'Scott',
+        surName: 'Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        firstName: 'Barbara',
+        surName: 'Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
